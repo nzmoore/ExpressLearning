1432383b37a4534d0a20ae2f67eabedb68ca25d2
Bill Moore <bill@nzmoore.com>
2017-04-25 16:56:01 -0400

    Using jade as template engine

--- Blueprints/server.js
+++ Blueprints/server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "jade");
+app.set("views", __dirname + "/views");
+
 app.get("/", function(req, res, next) {
-  res.send("yet another hello world from Express");
+  res.render("index");
 });
 
 app.listen(3000);
